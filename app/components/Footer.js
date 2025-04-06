e291bad4fc2bff348baad40440207f62e2434031
ryanDing26 <110417507+ryanDing26@users.noreply.github.com>
2025-04-06 16:07:27 -0700

    Fix minor spacing issue in footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,11 +4,11 @@ import { footerItems } from "../utils/content";
 export default function Footer() {
     return (
         // This is also where the Contact and Work sections link to
-        <footer className="lg:flex lg:justify-between text-center lg:text-2xl font-roboto font-semibold fade-in" id="contact"> 
-            <div className="pt-8">
+        <footer className="lg:flex lg:justify-between p-8 lg:p-0 text-center lg:text-2xl font-roboto font-semibold fade-in" id="contact"> 
+            <div className="lg:p-8 mb-8 lg:mb-0">
                 Made with ♡ by <a href="https://asgraphicstudio.ucsd.edu/" className="fancy-underline-white" target="_blank">A.S. Graphic Studio</a>
             </div>
-            <div className="flex justify-around p-8 md:p-0" id="work">
+            <div className="flex justify-around md:p-0" id="work">
                 {footerItems.map(({social, link}, index) => (
                     <a key={index} href={link} className="lg:m-8 fancy-underline-white" target="_blank">{social}</a>
                 ))}
